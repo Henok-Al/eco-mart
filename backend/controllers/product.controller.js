23577424f253395c79ac4dce0a32789920189455
Henok-Al <henok.gebresenbet@gmail.com>
2024-09-16 00:23:45 +0300

    delete product from both cloudinary and mongodb

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -67,3 +67,30 @@ export const createProduct = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+
+export const deleteProduct = async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id);
+
+		if (!product) {
+			return res.status(404).json({ message: "Product not found" });
+		}
+
+		if (product.image) {
+			const publicId = product.image.split("/").pop().split(".")[0];
+			try {
+				await cloudinary.uploader.destroy(`products/${publicId}`);
+				console.log("deleted image from cloduinary");
+			} catch (error) {
+				console.log("error deleting image from cloduinary", error);
+			}
+		}
+
+		await Product.findByIdAndDelete(req.params.id);
+
+		res.json({ message: "Product deleted successfully" });
+	} catch (error) {
+		console.log("Error in deleteProduct controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createProduct,
+  deleteProduct,
   getAllProducts,
   getFeaturedProducts,
 } from "../controllers/product.controller.js";
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
-router.get("/", protectRoute, adminRoute, createProduct);
+router.post("/", protectRoute, adminRoute, createProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
