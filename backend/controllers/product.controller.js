fa067c14b20bbd2c76fad27ce229b9dabb67abfb
Henok-Al <henok.gebresenbet@gmail.com>
2024-09-15 23:31:20 +0300

    featured products

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -4,8 +4,34 @@ export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find({}); //find all products
     res.json({ products });
-    
   } catch (error) {
     res.status(500).json({ message: "Server error: ", error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+
+    // if not in redis, fetch from mongodb
+    // .lean() is gonna return a plain javascript object instead of a mongodb document
+    // which is good for performance
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "No featured products found" });
+    }
+
+    // store in redis for future quick access
+
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+    res.json(featuredProducts);
+  } catch (error) {
+    console.log("Error in getFeaturedProducts controller", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
   }
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
-import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
+import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
+import {
+  protectRoute,
+  adminRoute,
+  
+} from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.get("/featured", getFeaturedProducts);
 
 export default router;
