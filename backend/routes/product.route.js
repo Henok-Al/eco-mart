557333dfa90f220bc74b32626dc4fd202d9d1483
Henok-Al <henok.gebresenbet@gmail.com>
2024-09-15 23:50:44 +0300

    setup cloudinary

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
 import {
-  protectRoute,
-  adminRoute,
-} from "../middleware/auth.middleware.js";
+  getAllProducts,
+  getFeaturedProducts,
+} from "../controllers/product.controller.js";
+import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
-router.get("/featured", getFeaturedProducts);
+router.get("/", protectRoute, adminRoute, createProduct);
 
 export default router;
