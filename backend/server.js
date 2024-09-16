45b0ea50c6ad49c7b067bd13b6b7bfab6ae3db55
Henok-Al <henok.gebresenbet@gmail.com>
2024-09-16 12:26:59 +0300

    implement cart route

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
-import ProductRoutes from "./routes/product.route.js";
-import cartRoute from "./routes/cart.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -15,8 +15,8 @@ app.use(express.json()); //allows you to parse the body of the request
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/products", ProductRoutes);
-app.use("/api/cart", cartRoute);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
