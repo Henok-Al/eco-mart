447e6aa9c6107cd94780270bc3de42936af4d5f9
Henok-Al <henok.gebresenbet@gmail.com>
2024-09-16 23:39:22 +0300

    setup react app

--- frontend/client.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const client = () => {
-  return <div>this is test</div>;
-};
-
-export default client;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+      {/* Background gradient */}
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0">
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]" />
+        </div>
+      </div>
+
+      <div className="relative z-50 pt-20">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
