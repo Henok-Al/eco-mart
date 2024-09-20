9fe660e6d596929a2e50f173f5361c54c81d7aea
Henok-Al <henok.gebresenbet@gmail.com>
2024-09-21 00:04:05 +0300

    fix bugs

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
-import { useUserStore } from "../store/useUserStore";
+import { useUserStore } from "../stores/useUserStore";
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
@@ -17,18 +17,17 @@ const SignUpPage = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     signup(formData);
-    console.log(formData);
   };
 
   return (
-    <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100 min-h-screen">
+    <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <motion.div
         className="sm:mx-auto sm:w-full sm:max-w-md"
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
       >
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-800">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-green-400">
           Create your account
         </h2>
       </motion.div>
@@ -39,12 +38,12 @@ const SignUpPage = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, delay: 0.2 }}
       >
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <div className="bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <form onSubmit={handleSubmit} className="space-y-6">
             <div>
               <label
                 htmlFor="name"
-                className="block text-sm font-medium text-gray-700"
+                className="block text-sm font-medium text-gray-300"
               >
                 Full name
               </label>
@@ -60,7 +59,8 @@ const SignUpPage = () => {
                   onChange={(e) =>
                     setFormData({ ...formData, name: e.target.value })
                   }
-                  className="block w-full px-3 py-2 pl-10 bg-gray-100 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+                  className="block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 rounded-md shadow-sm
+									 placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
                   placeholder="John Doe"
                 />
               </div>
@@ -69,7 +69,7 @@ const SignUpPage = () => {
             <div>
               <label
                 htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
+                className="block text-sm font-medium text-gray-300"
               >
                 Email address
               </label>
@@ -85,7 +85,10 @@ const SignUpPage = () => {
                   onChange={(e) =>
                     setFormData({ ...formData, email: e.target.value })
                   }
-                  className="block w-full px-3 py-2 pl-10 bg-gray-100 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+                  className=" block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 
+									rounded-md shadow-sm
+									 placeholder-gray-400 focus:outline-none focus:ring-green-500 
+									 focus:border-green-500 sm:text-sm"
                   placeholder="you@example.com"
                 />
               </div>
@@ -94,7 +97,7 @@ const SignUpPage = () => {
             <div>
               <label
                 htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
+                className="block text-sm font-medium text-gray-300"
               >
                 Password
               </label>
@@ -110,7 +113,8 @@ const SignUpPage = () => {
                   onChange={(e) =>
                     setFormData({ ...formData, password: e.target.value })
                   }
-                  className="block w-full px-3 py-2 pl-10 bg-gray-100 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+                  className=" block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 
+									rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
                   placeholder="••••••••"
                 />
               </div>
@@ -119,7 +123,7 @@ const SignUpPage = () => {
             <div>
               <label
                 htmlFor="confirmPassword"
-                className="block text-sm font-medium text-gray-700"
+                className="block text-sm font-medium text-gray-300"
               >
                 Confirm Password
               </label>
@@ -138,7 +142,8 @@ const SignUpPage = () => {
                       confirmPassword: e.target.value,
                     })
                   }
-                  className="block w-full px-3 py-2 pl-10 bg-gray-100 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+                  className=" block w-full px-3 py-2 pl-10 bg-gray-700 border
+									 border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
                   placeholder="••••••••"
                 />
               </div>
@@ -146,7 +151,10 @@ const SignUpPage = () => {
 
             <button
               type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition duration-150 ease-in-out disabled:opacity-50"
+              className="w-full flex justify-center py-2 px-4 border border-transparent 
+							rounded-md shadow-sm text-sm font-medium text-white bg-green-500
+							 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2
+							  focus:ring-green-500 transition duration-150 ease-in-out disabled:opacity-50"
               disabled={loading}
             >
               {loading ? (
@@ -166,11 +174,11 @@ const SignUpPage = () => {
             </button>
           </form>
 
-          <p className="mt-8 text-center text-sm text-green-600">
+          <p className="mt-8 text-center text-sm text-gray-400">
             Already have an account?{" "}
             <Link
               to="/login"
-              className="font-medium text-green-500 hover:text-green-600"
+              className="font-medium text-green-400 hover:text-green-300"
             >
               Login here <ArrowRight className="inline h-4 w-4" />
             </Link>
