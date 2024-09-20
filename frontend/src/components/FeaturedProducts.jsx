8227b653bccdf9dba361d338927793daec664215
Henok-Al <henok.gebresenbet@gmail.com>
2024-09-20 16:15:09 +0300

    reuse the product card component

--- frontend/src/components/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts.jsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
-import { ShoppingCart, ChevronLeft, ChevronRight } from "lucide-react";
-import { useCartStore } from "../store/useCartStore";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import ProductCard from "./ProductCard";
 
 const FeaturedProducts = ({ featuredProducts }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [itemsPerPage, setItemsPerPage] = useState(4);
-
-  const { addToCart } = useCartStore();
 
   useEffect(() => {
     const handleResize = () => {
@@ -22,11 +20,15 @@ const FeaturedProducts = ({ featuredProducts }) => {
   }, []);
 
   const nextSlide = () => {
-    setCurrentIndex((prevIndex) => prevIndex + itemsPerPage);
+    if (currentIndex + itemsPerPage < featuredProducts.length) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    }
   };
 
   const prevSlide = () => {
-    setCurrentIndex((prevIndex) => prevIndex - itemsPerPage);
+    if (currentIndex > 0) {
+      setCurrentIndex((prevIndex) => prevIndex - 1);
+    }
   };
 
   const isStartDisabled = currentIndex === 0;
@@ -44,40 +46,17 @@ const FeaturedProducts = ({ featuredProducts }) => {
               className="flex transition-transform duration-300 ease-in-out"
               style={{
                 transform: `translateX(-${
-                  currentIndex * (100 / itemsPerPage)
+                  (currentIndex * 100) / itemsPerPage
                 }%)`,
+                width: `${(100 / itemsPerPage) * featuredProducts.length}%`,
               }}
             >
-              {featuredProducts?.map((product) => (
+              {featuredProducts.map((product) => (
                 <div
                   key={product._id}
                   className="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 flex-shrink-0 px-2"
                 >
-                  <div className="bg-white bg-opacity-10 backdrop-blur-sm rounded-lg shadow-lg overflow-hidden h-full transition-all duration-300 hover:shadow-xl border border-gray-500/30">
-                    <div className="overflow-hidden">
-                      <img
-                        src={product.image}
-                        alt={product.name}
-                        className="w-full h-48 object-cover transition-transform duration-300 ease-in-out hover:scale-110"
-                      />
-                    </div>
-                    <div className="p-4">
-                      <h3 className="text-lg font-semibold mb-2 text-gray-800">
-                        {product.name}
-                      </h3>
-                      <p className="text-gray-800 font-medium mb-4">
-                        ${product.price.toFixed(2)}
-                      </p>
-                      <button
-                        onClick={() => addToCart(product)}
-                        className="w-full bg-gray-600 hover:bg-gray-500 text-white font-semibold py-2 px-4 rounded transition-colors duration-300 
-												flex items-center justify-center"
-                      >
-                        <ShoppingCart className="w-5 h-5 mr-2" />
-                        Add to Cart
-                      </button>
-                    </div>
-                  </div>
+                  <ProductCard product={product} />
                 </div>
               ))}
             </div>
@@ -110,4 +89,5 @@ const FeaturedProducts = ({ featuredProducts }) => {
     </div>
   );
 };
+
 export default FeaturedProducts;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,9 +5,9 @@ import { useProductStore } from "../store/useProductStore";
 
 const categories = [
   { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
-  { href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
+  { href: "/t-shirts", name: "T-shirts", imageUrl: "/t-shirts.jpg" },
   { href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
-  { href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
+  { href: "/glasses", name: "Glasses", imageUrl: "/glasses.jpg" },
   { href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
   { href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
   { href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
@@ -23,7 +23,7 @@ const HomePage = () => {
   return (
     <div className="relative min-h-screen text-white overflow-hidden">
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
-        <h1 className="text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4">
+        <h1 className="text-center text-5xl sm:text-6xl font-bold text-gray-400 mb-4">
           Explore Our Categories
         </h1>
         <p className="text-center text-xl text-gray-300 mb-12">
