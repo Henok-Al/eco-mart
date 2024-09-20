2b9323132a748274d5f3a8e44e03f9f611031a69
Henok-Al <henok.gebresenbet@gmail.com>
2024-09-21 02:23:28 +0300

    fix signup

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
-import { useUserStore } from "../stores/useUserStore";
+import { useUserStore } from "../store/useUserStore";
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
